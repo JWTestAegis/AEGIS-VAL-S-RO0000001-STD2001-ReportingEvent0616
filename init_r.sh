d58891f9bba66ff7577f46ffff9f7965bf14e253
JiangWei2025 <daniel.jiang.w@foxmail.com>
2026-06-16 12:40:40 +0800

    Update init_r.sh

--- init_r.sh
+++ init_r.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # **************************************************************
 # Copy .Rprofile and .Renviron to project directory
-# updated for test
+# updated for test 2
 # **************************************************************
 echo "Current Directory: $(pwd)"
 echo "*** File list under project directory BEFORE copying ***"
